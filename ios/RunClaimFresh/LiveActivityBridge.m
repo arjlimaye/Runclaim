400ee28ddbbaa9c09963855c1a2937598656c812
Arjun Limaye <arjunlimaye@Arjuns-MacBook-Air-2.local>
2026-05-24 23:26:41 +0530

    Fix LiveActivityBridge.m to match 3-param Swift signatures

--- ios/RunClaimFresh/LiveActivityBridge.m
+++ ios/RunClaimFresh/LiveActivityBridge.m
@@ -2,8 +2,8 @@
 
 @interface RCT_EXTERN_MODULE(LiveActivityBridge, NSObject)
 
-RCT_EXTERN_METHOD(startActivity:(double)elapsedSeconds distanceKm:(double)distanceKm)
-RCT_EXTERN_METHOD(updateActivity:(double)elapsedSeconds distanceKm:(double)distanceKm)
+RCT_EXTERN_METHOD(startActivity:(double)elapsedSeconds distanceKm:(double)distanceKm startTimestamp:(double)startTimestamp)
+RCT_EXTERN_METHOD(updateActivity:(double)elapsedSeconds distanceKm:(double)distanceKm startTimestamp:(double)startTimestamp)
 RCT_EXTERN_METHOD(endActivity)
 
 + (BOOL)requiresMainQueueSetup { return NO; }
